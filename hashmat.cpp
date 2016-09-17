a30a5909c14772df356396ec1adc2d913bb1aee6
John-Sixto-Rodriguez <john62rodriguez@yahoo.com>
2016-09-17 09:48:37 +0800

    Add files via upload

---
+++ hashmat.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+int main()
+{
+	int a,b,res,res1,res2;
+	
+	cout<<"Enter the number of soldiers in Hashmat's army and his opponent's army:";
+	for(a=1;a<=2^32;a++)
+	{
+		
+	
+	if(a<=2^32 && b<=2^32)
+		
+		cin>>a>>b;
+		res=(a-b);
+		abs(res);
+	
+	
+		cin>>a>>b;
+		res1=(a-b);
+		abs(res1);
+	
+		cin>>a>>b;
+		res2=(a-b);
+		abs(res2);
+	
+		cout<<"\nThe difference of number of soldiers between Hashmat's army and his opponent's army:"<<endl;
+	
+		cout<<abs(res)<<endl;
+		cout<<abs(res1)<<endl;
+		cout<<abs(res2)<<endl;
+	
+
+		return 0;
+	}	
+		
+		if(a>2^32 && b>2^32)
+		{
+		
+			cout<<"End of File";
+		
+		}
+		
+	
+	
+	return 0;
+}
+
+
+
